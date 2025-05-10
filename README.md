63d23f8891659a8e8e97f93f2d761e8dbd56e083
arham10101 <187818588+mian-arham-haroon@users.noreply.github.com>
2025-05-10 12:04:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-App
+A responsive weather forecast application built with Python’s Tkinter GUI. It displays real-time weather data using OpenWeatherMap API, including temperature, wind, humidity, description, pressure, and local time based on the user's city. Designed with intuitive visuals and interactive UI for learning and practical use in weather-based projects.
---
+++ main.py
@@ -0,0 +1,99 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+
+root=Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
+
+def getWeather():
+    city=textfield.get()
+
+    geolocator=Nominatim(user_agent="geoapiexericses")
+    location=geolocator.geocode(city)
+    obj=TimezoneFinder()
+    result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
+
+    home=pytz.timezone(result)
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M %p")
+    clock.config(text=current_time)
+    name.config(text="CURRENT WEATHER")
+
+    apikey="7ab842e32ff35851296182f2541c0de7"
+    #wearher 
+    api=(f"https://api.openweathermap.org/data/2.5/weather?{city}&appid={apikey}")
+    json_data=requests.get(api).json
+    condition=json_data['weather'][0]['main']
+    description=json_data['weather'][0]['description']
+    temp = int(json_data['main']['temp']-273.15)
+    pressuer=json_data['main']['pressuer']
+    humidity=json_data['main']['humidity']
+    wind=json_data['wind']['speed']
+
+    t.config(text=(temp,"°"))
+    c.config(text=(condition,"|","FEELS","LIKE",temp,"°"))    
+
+
+
+# search box
+Search_image = PhotoImage(file="search.png")
+myimage=Label(image=Search_image)
+myimage.place(x=20,y=20)
+
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),border=0,bg="#404040",fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+# search icon
+Search_icon=PhotoImage(file="search_icon.png")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
+myimage_icon.place(x=400,y=34)
+
+#logo inscreen
+logo_iage=PhotoImage(file="logo.png")
+logo=Label(image=logo_iage)
+logo.place(x=150,y=100)
+
+#Botton box
+Frame_image=PhotoImage(file="box.png")
+frame_myimage=Label(image=Frame_image)
+frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+#time
+name=Label(root,font=("arial",15,"bold"))
+name.place(x=30,y=100)
+clock=Label(root,font=("helvetica",15,"bold"))
+clock.place(x=30,y=130)
+#label
+label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=120,y=400)
+label1=Label(root,text="HUMIDITY",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=250,y=400)
+label1=Label(root,text="DESCRIPTION",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=430,y=400)
+label1=Label(root,text="PREESER",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=650,y=400)
+
+t=Label(font=("arial",70,"bold"),fg="#ee666d")
+t.place(x=400,y=150)
+c=Label(font=("arial",15,"bold"))
+c.place(x=400,y=250)
+
+w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+w.place(x=120,y=430)
+h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+h.place(x=250,y=430)
+d=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+d.place(x=450,y=430)
+p=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+p.place(x=670,y=430)
+
+
+root.mainloop()
